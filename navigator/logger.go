832c55e3956ca11daa2c66542a62f4b6ebd53fcb
H0llyW00dzZ <priv8@btz.pm>
2023-12-22 05:44:09 +0700

    Feat [Navigator] [Dependency Injection] Custom Log
    - [+] chore(logger.go): add comments explaining LogFieldOption and WithAnyZapField functions
    - [+] feat(logger.go): add WithAnyZapField function to allow for custom zap.Field in log entries

--- navigator/logger.go
+++ navigator/logger.go
@@ -8,6 +8,17 @@ import (
 	"go.uber.org/zap"
 )
 
+// LogFieldOption is a type that represents a function which returns a zap.Field.
+// It's used to pass custom field generators to functions that create a slice of zap.Fields for logging.
+// This allows for deferred creation of zap.Fields, which can be useful when the value of the field
+// is not immediately available or when you want to include custom logic to determine the value of the field.
+//
+// Example usage:
+//
+//	fields := navigator.CreateLogFields("sailing", "the high seas", navigator.WithAnyZapField(zap.Int("treasure chests", 5)))
+//	navigator.LogInfoWithEmoji("🏴‍☠️", "Pirates ahoy!", fields...)
+type LogFieldOption func() zap.Field
+
 // Logger is a package-level variable to access the zap logger throughout the worker package.
 var Logger *zap.Logger
 
@@ -45,6 +56,26 @@ func LogErrorWithEmoji(emoji string, context string, fields ...zap.Field) {
 		return
 	}
 	Logger.Error(emoji+" "+context, fields...)
+}
+
+// WithAnyZapField creates a LogFieldOption that encapsulates a zap.Field for deferred addition to a log entry.
+// This function is particularly handy when you have a custom field to add to your log that isn't
+// already covered by existing "With*" functions. It allows for a more flexible and dynamic approach
+// to logging, akin to how a pirate might prefer the freedom to navigate the open seas.
+//
+// Example:
+//
+// Let's say we want to log an event related to a pirate's treasure map. We have a custom binary field
+// that represents the map, and we want to include this in our log fields. We can use WithAnyZapField
+// to add this custom field to our logs as follows:
+//
+//	treasureMap := []byte{0x0A, 0x0B, 0x0C, 0x0D} // This represents our treasure map in binary form.
+//	fields := navigator.CreateLogFields("sailing", "find treasure", navigator.WithAnyZapField(zap.Binary("treasureMap", treasureMap)))
+//	navigator.LogInfoWithEmoji("🏴‍☠️", "Found a treasure map!", fields...)
+func WithAnyZapField(field zap.Field) LogFieldOption {
+	return func() zap.Field {
+		return field
+	}
 }
 
 // createLogFields creates a slice of zap.Field with the operation and additional info.
