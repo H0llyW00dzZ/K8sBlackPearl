df2d40c1a1b6eb9d1ff3c16652808ecbd1f06a4d
H0llyW00dzZ <priv8@btz.pm>
2023-12-21 18:20:42 +0700

    Feat [Navigator] [Language] [dependabot]
    - [+] chore(dependabot.yml): add dependabot configuration file
    - [+] chore(go.mod): add Go module dependencies
    - [+] chore(go.sum): add Go module checksums
    - [+] feat(language/init_const.go): add language package constants
    - [+] feat(navigator/logger.go): add logger functionality to navigator package

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/H0llyW00dzZ/K8sBlackPearl
+
+go 1.21.5
+
+require go.uber.org/zap v1.26.0
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ language/init_const.go
@@ -0,0 +1,35 @@
+package language
+
+const (
+	ErrorListingPods       = "error listing pods: %w"
+	ErrorUpdatingPodLabels = "error updating pod labels: %w"
+	ErrorCreatingPod       = "error creating pod: %w"
+	ErrorDeletingPod       = "error deleting pod: %w"
+	ErrorGettingPod        = "error getting pod: %w"
+	ErrorPodNotFound       = "pod not found"
+	ErrorRetrievingPods    = "Error retrieving pods: %w"
+	PodAndStatus           = "Pod: %s, Status: %s"
+	PodAndStatusAndHealth  = "Pod: %s, Status: %s, Health: %s"
+	NotHealthyStatus       = "Not Healthy"
+	HealthyStatus          = "Healthy"
+	WorkerCancelled        = "Worker cancelled: %v"
+	HOME                   = "HOME"
+	kube                   = ".kube"
+	Config                 = "config"
+	errconfig              = "cannot load kubeconfig: %w"
+	cannotcreatek8s        = "cannot create kubernetes client: %w"
+)
+
+const (
+	FetchingPods   = "Fetching pods"
+	PodsFetched    = "Pods fetched"
+	ProcessingPods = "Processing pods"
+)
+
+const (
+	TaskLabelKey    = "LabelKey"
+	TaskCheckHealth = "CheckHealth"
+	TaskGetPod      = "GetPod"
+	TaskFetchPods   = "FetchPods"
+	TaskProcessPod  = "ProcessPod"
+)
