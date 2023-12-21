20f125ea175ce1a0d1d2246ef3d765a3905f8cd7
H0llyW00dzZ <priv8@btz.pm>
2023-12-21 19:55:24 +0700

    Feat [Worker] Init Setup
    - [+] chore(go.mod): add github.com/imdario/mergo and github.com/spf13/pflag as indirect dependencies
    - [+] chore(go.sum): add checksums for github.com/imdario/mergo and github.com/spf13/pflag
    - [+] refactor(init_const.go): remove unused constants HOME, kube, and Config
    - [+] feat(worker): add cmd_constant.go file with constants and errors
    - [+] feat(worker): add setup.go file with NewKubernetesClient function

---
+++ worker/cmd_constant.go
@@ -0,0 +1,14 @@
+package worker
+
+// defined in worker/cmd_constant.go
+const (
+	HOME    = "HOME"
+	dotkube = ".kube"
+	Config  = "config"
+)
+
+// defined error in worker/cmd_constant.go
+const (
+	errconfig       = "cannot load kubeconfig: %w"
+	cannotcreatek8s = "cannot create kubernetes client: %w"
+)
