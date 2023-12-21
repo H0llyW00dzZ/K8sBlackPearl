34322daa5bc4e2956c4c174768e6a16514a0f7b8
H0llyW00dzZ <priv8@btz.pm>
2023-12-21 23:46:35 +0700

    Refactor Worker
    - [+] feat(navigator/logger.go): refactor logger package to use a package-level Logger variable and add thread-safe access to it
    - [+] fix(worker/captain.go): update logger variable access to use navigator.Logger instead of worker.Logger
    - [+] fix(worker/crew.go): update logger variable access to use navigator.Logger instead of worker.Logger
    - [+] fix(worker/logger.go): delete unused logger package

--- navigator/logger.go
+++ navigator/logger.go
@@ -2,31 +2,54 @@ package navigator
 
 import (
 	"fmt"
+	"sync"
 
+	"github.com/H0llyW00dzZ/K8sBlackPearl/language"
 	"go.uber.org/zap"
 )
 
-// Logger is a package-level variable to access the zap logger throughout the handlers package.
-// It is intended to be used by other functions within the package for logging purposes.
-var navigator *zap.Logger
+// Logger is a package-level variable to access the zap logger throughout the worker package.
+var Logger *zap.Logger
 
-// SetLogger sets the logger instance for the package.
+// mu is used to protect access to the Logger variable to make it safe for concurrent use.
+var mu sync.Mutex
+
+// SetLogger sets the logger instance for the package in a thread-safe manner.
 func SetLogger(logger *zap.Logger) {
-	navigator = logger
+	mu.Lock()
+	defer mu.Unlock()
+	Logger = logger
 }
 
-// logInfoWithEmoji logs an informational message with given emoji, context, and fields.
-func logInfoWithEmoji(emoji string, context string, fields ...zap.Field) {
-	navigator.Info(emoji+"  "+context, fields...)
+// LogInfoWithEmoji logs an informational message with a given emoji, context, and fields.
+// It checks if the Logger is not nil before logging to prevent panics.
+func LogInfoWithEmoji(emoji string, context string, fields ...zap.Field) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if Logger == nil {
+		fmt.Printf(language.ErrorLoggerIsNotSet, context)
+		return
+	}
+	Logger.Info(emoji+" "+context, fields...)
 }
 
-// logErrorWithEmoji logs an error message with given emoji, context, and fields.
-func logErrorWithEmoji(emoji string, context string, fields ...zap.Field) {
-	navigator.Error(emoji+"  "+context, fields...)
+// logErrorWithEmoji logs an error message with a given emoji, context, and fields.
+// It checks if the Logger is not nil before logging to prevent panics.
+func LogErrorWithEmoji(emoji string, context string, fields ...zap.Field) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if Logger == nil {
+		fmt.Printf(language.ErrorLoggerIsNotSet, context)
+		return
+	}
+	Logger.Error(emoji+" "+context, fields...)
 }
 
 // createLogFields creates a slice of zap.Field with the operation and additional info.
-func createLogFields(sailing string, shipsnamespace string, infos ...string) []zap.Field {
+// It can be used to add structured context to logs.
+func CreateLogFields(sailing string, shipsnamespace string, infos ...string) []zap.Field {
 	fields := []zap.Field{
 		zap.String("sailing", sailing),
 		zap.String("shipsnamespace", shipsnamespace),
