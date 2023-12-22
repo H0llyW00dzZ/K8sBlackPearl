1cfb88f4574b974762980e8367161aa7e3f61228
H0llyW00dzZ <priv8@btz.pm>
2023-12-22 23:55:41 +0700

    Feat [Worker] [Task] [List Options] Code Comment
    - [+] docs(list_options.go): add documentation for getListOptions function

--- worker/list_options.go
+++ worker/list_options.go
@@ -7,6 +7,19 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// getListOptions constructs a ListOptions struct from a map of parameters.
+// It extracts the 'labelSelector', 'fieldSelector', and 'limit' values from the parameters map,
+// performing type assertions as needed. This function is designed to parse and validate
+// the parameters required for listing Kubernetes resources.
+//
+// Parameters:
+//   - parameters: A map containing the keys and values for constructing the ListOptions.
+//     Expected keys are 'labelSelector', 'fieldSelector', and 'limit'.
+//
+// Returns:
+//   - A v1.ListOptions struct initialized with the values from the parameters map.
+//   - An error if any of the required parameters are missing or if the type assertion fails,
+//     indicating invalid or malformed input.
 func getListOptions(parameters map[string]interface{}) (v1.ListOptions, error) {
 	labelSelector, labelOk := parameters["labelSelector"].(string)
 	fieldSelector, fieldOk := parameters["fieldSelector"].(string)
