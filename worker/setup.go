0d7a5387f79b6dbc100ef0d1187de4f65e4fd06f
H0llyW00dzZ <priv8@btz.pm>
2023-12-24 02:29:55 +0700

    Refactor [Worker] [k8s] improve error handling
    - [+] refactor(worker): refactor constant names and error messages in cmd_constant.go
    - [+] feat(worker): add comments and improve error handling in NewKubernetesClient function in setup.go

--- worker/setup.go
+++ worker/setup.go
@@ -10,23 +10,32 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// NewKubernetesClient creates a new Kubernetes client using the in-cluster configuration.
-// This is typically used when the application itself is running within a Kubernetes cluster.
+// NewKubernetesClient creates a new Kubernetes client using either the in-cluster configuration
+// when running within a Kubernetes cluster, or the kubeconfig file when running outside.
+//
+// Returns:
+//   - A pointer to a kubernetes.Clientset ready to use for interactions with the Kubernetes API.
+//   - An error if the configuration could not be determined or the client could not be created.
 func NewKubernetesClient() (*kubernetes.Clientset, error) {
-	// Use the current context in kubeconfig
+	// Attempt to use the in-cluster configuration.
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		// If running outside the cluster, use the kubeconfig file.
-		kubeconfig := filepath.Join(os.Getenv(HOME), dotkube, Config)
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		// If the in-cluster configuration is not found, fall back to the kubeconfig file.
+		homeDir, found := os.LookupEnv(homeEnvVar)
+		if !found {
+			return nil, fmt.Errorf(errEnvVar, homeEnvVar)
+		}
+		kubeconfigPath := filepath.Join(homeDir, dotKubeDir, kubeConfigFile)
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 		if err != nil {
-			return nil, fmt.Errorf(errconfig, err)
+			return nil, fmt.Errorf(errConfig, err)
 		}
 	}
 
+	// Create the Kubernetes clientset with the appropriate configuration.
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		return nil, fmt.Errorf(cannotcreatek8s, err)
+		return nil, fmt.Errorf(cannotCreateK8s, err)
 	}
 
 	return clientset, nil
