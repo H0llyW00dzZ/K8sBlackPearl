85dc8cc1ad178a013d7339744ba19c36d98c93fe
H0llyW00dzZ <priv8@btz.pm>
2023-12-24 02:21:19 +0700

    Docs [Worker] [Init] implementations docs
    - [+] feat(worker): register TaskRunner implementations for worker tasks
     - Register TaskRunner for retrieving Kubernetes pods with the name "CrewGetPods"
     - Register TaskRunner for checking the health of Kubernetes pods with the name "CrewCheckHealthPods"
     - Register TaskRunner for an alternate method of retrieving Kubernetes pods with the name "CrewGetPodsTaskRunner"

--- worker/init.go
+++ worker/init.go
@@ -1,9 +1,20 @@
 package worker
 
-// list worker tasks
+// init registers available TaskRunner implementations for worker tasks.
+// This setup allows the worker package to instantiate specific task runners
+// based on task names, facilitating a dynamic task execution environment.
 func init() {
+	// RegisterTaskRunner associates a string identifier with a TaskRunner constructor.
+	// When a task with the corresponding name is processed, the associated constructor
+	// is used to create an instance of the TaskRunner to execute the task.
+
+	// Registers a TaskRunner for retrieving Kubernetes pods.
 	RegisterTaskRunner("CrewGetPods", func() TaskRunner { return &CrewGetPods{} })
+
+	// Registers a TaskRunner for checking the health of Kubernetes pods.
 	RegisterTaskRunner("CrewCheckHealthPods", func() TaskRunner { return &CrewProcessCheckHealthTask{} })
+
+	// Registers a TaskRunner for an alternate method of retrieving Kubernetes pods.
 	RegisterTaskRunner("CrewGetPodsTaskRunner", func() TaskRunner { return &CrewGetPodsTaskRunner{} })
 
 }
