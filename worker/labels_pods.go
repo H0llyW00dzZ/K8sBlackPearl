c835320b70098049cd7f7be2b4d81fcd361ecad2
H0llyW00dzZ <priv8@btz.pm>
2023-12-24 09:28:40 +0700

    Feat [Worker] [Labels Pods] Resource Version
    - [+] feat(labels_pods.go): add labelSinglePodWithResourceVersion function to apply label to a single pod if it doesn't already have it
    - [+] feat(labels_pods.go): add fetchLatestPodVersion function to retrieve the most recent version of the pod
    - [+] feat(labels_pods.go): add shouldUpdatePod function to determine if the pod's labels need to be updated
    - [+] feat(labels_pods.go): add updatePodLabels function to apply the update to the pod's labels using a strategic merge patch
    - [+] feat(labels_pods.go): add getUpdatedLabels function to construct a new labels map containing the updated label
    - [+] feat(labels_pods.go): add wrapPodError function to enrich the provided error with additional context

--- worker/labels_pods.go
+++ worker/labels_pods.go
@@ -2,13 +2,136 @@ package worker
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/H0llyW00dzZ/K8sBlackPearl/language"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 )
+
+// labelSinglePodWithResourceVersion applies the label to a single pod if it doesn't already have it.
+// It fetches the latest version of the pod to ensure the update is based on the current state of the pod.
+//
+// Parameters:
+//   - ctx: A context.Context for managing cancellation and deadlines.
+//   - clientset: A *kubernetes.Clientset instance used to interact with the Kubernetes API.
+//   - podName: The name of the pod to label.
+//   - namespace: The namespace in which the pod is located.
+//   - labelKey: The key of the label to be added or updated.
+//   - labelValue: The value for the label.
+//
+// Returns:
+//   - An error if the pod cannot be retrieved or updated with the new label.
+func labelSinglePodWithResourceVersion(ctx context.Context, clientset *kubernetes.Clientset, podName, namespace, labelKey, labelValue string) error {
+	latestPod, err := fetchLatestPodVersion(ctx, clientset, podName, namespace)
+	if err != nil {
+		return wrapPodError(podName, err)
+	}
+
+	if shouldUpdatePod(latestPod, labelKey, labelValue) {
+		return updatePodLabels(ctx, clientset, latestPod, namespace, podName, labelKey, labelValue)
+	}
+
+	return nil
+}
+
+// fetchLatestPodVersion retrieves the most recent version of the pod from the Kubernetes API.
+// This is necessary to avoid conflicts when updating the pod's labels.
+//
+// Parameters:
+//   - ctx: A context.Context for managing cancellation and deadlines.
+//   - clientset: A *kubernetes.Clientset instance used to interact with the Kubernetes API.
+//   - podName: The name of the pod to retrieve.
+//   - namespace: The namespace in which the pod is located.
+//
+// Returns:
+//   - A pointer to the retrieved corev1.Pod instance.
+//   - An error if the pod cannot be retrieved.
+func fetchLatestPodVersion(ctx context.Context, clientset *kubernetes.Clientset, podName, namespace string) (*corev1.Pod, error) {
+	return clientset.CoreV1().Pods(namespace).Get(ctx, podName, v1.GetOptions{})
+}
+
+// shouldUpdatePod determines if the pod's labels need to be updated with the new labelKey and labelValue.
+// It compares the existing labels of the pod to the desired label.
+//
+// Parameters:
+//   - pod: A pointer to the corev1.Pod instance to check.
+//   - labelKey: The key of the label to be added or updated.
+//   - labelValue: The value for the label.
+//
+// Returns:
+//   - True if the pod needs to be updated, false otherwise.
+func shouldUpdatePod(pod *corev1.Pod, labelKey, labelValue string) bool {
+	return pod.Labels[labelKey] != labelValue
+}
+
+// updatePodLabels applies the update to the pod's labels using a strategic merge patch.
+// It ensures that only the labels are updated, leaving the rest of the pod configuration unchanged.
+//
+// Parameters:
+//   - ctx: A context.Context for managing cancellation and deadlines.
+//   - clientset: A *kubernetes.Clientset instance used to interact with the Kubernetes API.
+//   - pod: A pointer to the corev1.Pod instance to update.
+//   - namespace: The namespace in which the pod is located.
+//   - podName: The name of the pod to update.
+//   - labelKey: The key of the label to be added or updated.
+//   - labelValue: The value for the label.
+//
+// Returns:
+//   - An error if the patch cannot be created or applied to the pod.
+func updatePodLabels(ctx context.Context, clientset *kubernetes.Clientset, pod *corev1.Pod, namespace, podName, labelKey, labelValue string) error {
+	pod.Labels = getUpdatedLabels(pod.Labels, labelKey, labelValue)
+
+	patchData, err := json.Marshal(map[string]interface{}{
+		"metadata": map[string]interface{}{
+			"labels": pod.Labels,
+		},
+	})
+	if err != nil {
+		return wrapPodError(podName, err)
+	}
+
+	_, err = clientset.CoreV1().Pods(namespace).Patch(ctx, podName, types.StrategicMergePatchType, patchData, v1.PatchOptions{})
+	if err != nil {
+		return wrapPodError(podName, err)
+	}
+
+	return nil
+}
+
+// getUpdatedLabels constructs a new labels map containing the updated label.
+// If the original labels map is nil, it initializes a new map before adding the label.
+//
+// Parameters:
+//   - labels: The original map of labels to update.
+//   - labelKey: The key of the label to be added or updated.
+//   - labelValue: The value for the label.
+//
+// Returns:
+//   - A new map of labels with the updated label included.
+func getUpdatedLabels(labels map[string]string, labelKey, labelValue string) map[string]string {
+	if labels == nil {
+		labels = make(map[string]string)
+	}
+	labels[labelKey] = labelValue
+	return labels
+}
+
+// wrapPodError enriches the provided error with additional context, specifically mentioning the pod name.
+// This helps in identifying which pod encountered the error when multiple pods are being processed.
+//
+// Parameters:
+//   - podName: The name of the pod related to the error.
+//   - err: The original error to wrap with additional context.
+//
+// Returns:
+//   - An error that includes the pod name and the original error message.
+func wrapPodError(podName string, err error) error {
+	return fmt.Errorf(language.ErrorFailedToUpdateLabelSPods, podName, err)
+}
 
 // LabelPods sets a specific label on all pods within the specified namespace that do not already have it.
 // This function iterates over all pods in the namespace and delegates the labeling of each individual pod
@@ -32,7 +155,7 @@ func LabelPods(ctx context.Context, clientset *kubernetes.Clientset, namespace, 
 
 	// Iterate over the list of pods and update their labels if necessary.
 	for _, pod := range pods.Items {
-		if err := labelSinglePod(ctx, clientset, &pod, namespace, labelKey, labelValue); err != nil {
+		if err := labelSinglePodWithResourceVersion(ctx, clientset, pod.Name, namespace, labelKey, labelValue); err != nil {
 			return err
 		}
 	}
