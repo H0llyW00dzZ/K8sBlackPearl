308751acf92b13661bcae60b7a2261fbacc1ca33
H0llyW00dzZ <priv8@btz.pm>
2023-12-25 11:09:23 +0700

    Reduce [Setup] [K8S] Cyclomatic
    - [+] refactor(worker/setup.go): simplify NewKubernetesClient function and extract buildOutOfClusterConfig function
    - [+] feat(worker/setup.go): add buildOutOfClusterConfig function to build configuration from kubeconfig file

--- worker/setup.go
+++ worker/setup.go
@@ -10,33 +10,35 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// NewKubernetesClient creates a new Kubernetes client using either the in-cluster configuration
-// when running within a Kubernetes cluster, or the kubeconfig file when running outside.
+// NewKubernetesClient creates a new Kubernetes client using the in-cluster configuration
+// or the kubeconfig file, depending on the environment.
 //
 // Returns:
-//   - A pointer to a kubernetes.Clientset ready to use for interactions with the Kubernetes API.
-//   - An error if the configuration could not be determined or the client could not be created.
+//   - A pointer to a kubernetes.Clientset ready for Kubernetes API interactions.
+//   - An error if the configuration fails or the client cannot be created.
 func NewKubernetesClient() (*kubernetes.Clientset, error) {
-	// Attempt to use the in-cluster configuration.
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		// If the in-cluster configuration is not found, fall back to the kubeconfig file.
-		homeDir, found := os.LookupEnv(homeEnvVar)
-		if !found {
-			return nil, fmt.Errorf(errEnvVar, homeEnvVar)
-		}
-		kubeconfigPath := filepath.Join(homeDir, dotKubeDir, kubeConfigFile)
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		config, err = buildOutOfClusterConfig()
 		if err != nil {
-			return nil, fmt.Errorf(errConfig, err)
+			return nil, err
 		}
 	}
 
-	// Create the Kubernetes clientset with the appropriate configuration.
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf(cannotCreateK8s, err)
+	return kubernetes.NewForConfig(config)
+}
+
+// buildOutOfClusterConfig attempts to build a configuration from the kubeconfig file.
+//
+// Returns:
+//   - A configuration object for the Kubernetes client.
+//   - An error if the kubeconfig file cannot be found or is invalid.
+func buildOutOfClusterConfig() (*rest.Config, error) {
+	homeDir, found := os.LookupEnv(homeEnvVar)
+	if !found {
+		return nil, fmt.Errorf(errEnvVar, homeEnvVar)
 	}
 
-	return clientset, nil
+	kubeconfigPath := filepath.Join(homeDir, dotKubeDir, kubeConfigFile)
+	return clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 }
