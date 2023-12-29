31d0557e7d90e930120e75ceb5e0745ccba0af62
H0llyW00dzZ <priv8@btz.pm>
2023-12-30 01:20:57 +0700

    Fix Worker formatting parameters  (#53)
    * Chore Git Ignore
    
    - [+] chore(.gitignore): add test.json, test.yaml, and test.yml to ignore list
    - [+] chore(.gitignore): remove debug.go from ignore list
    
    * Fix [Worker] [List Options Pods] formatting parameters
    
    - [+] fix(worker): handle both int and float64 types for 'limit' parameter in getListOptions function
    
    * Chore [Language] Update Constants
    
    - [+] chore(init_const.go): add error messages for parameter validation
    
    * Refactor [Worker] [List Options Pods] Reduce Cyclomatic
    
    - [+] refactor(list_options.go): improve getListOptions function to handle parameter retrieval and type assertion errors
    - [+] feat(list_options.go): add getParamAsString and getParamAsInt64 helper functions for retrieving string and integer values from a map

--- worker/list_options.go
+++ worker/list_options.go
@@ -8,38 +8,69 @@ import (
 )
 
 // getListOptions constructs a ListOptions struct from a map of parameters.
-// It extracts the 'labelSelector', 'fieldSelector', and 'limit' values from the parameters map,
-// performing type assertions as needed. This function is designed to parse and validate
-// the parameters required for listing Kubernetes resources.
+// It extracts 'labelSelector', 'fieldSelector', and 'limit' from the map.
+// This function is designed to parse and validate the parameters required for listing Kubernetes resources.
 //
-// Parameters:
-//   - parameters: A map containing the keys and values for constructing the ListOptions.
-//     Expected keys are 'labelSelector', 'fieldSelector', and 'limit'.
+// params - a map containing the keys and values for constructing the ListOptions.
 //
-// Returns:
-//   - A v1.ListOptions struct initialized with the values from the parameters map.
-//   - An error if any of the required parameters are missing or if the type assertion fails,
-//     indicating invalid or malformed input.
+//	Expected keys are 'labelSelector', 'fieldSelector', and 'limit'.
+//
+// Returns a v1.ListOptions struct initialized with the values from the parameters map,
+// and an error if any of the required parameters are missing or if the type assertion fails.
 func getListOptions(params map[string]interface{}) (v1.ListOptions, error) {
-	listOptions := v1.ListOptions{}
-
-	labelSelector, ok := params[labelSelector].(string)
-	if !ok {
-		return listOptions, fmt.Errorf(language.ErrorParamLabelSelector)
+	labelSelector, err := getParamAsString(params, labelSelector)
+	if err != nil {
+		return v1.ListOptions{}, fmt.Errorf(language.ErrorParamLabelSelector)
 	}
-	listOptions.LabelSelector = labelSelector
 
-	fieldSelector, ok := params[fieldSelector].(string)
-	if !ok {
-		return listOptions, fmt.Errorf(language.ErrorParamFieldSelector)
+	fieldSelector, err := getParamAsString(params, fieldSelector)
+	if err != nil {
+		return v1.ListOptions{}, fmt.Errorf(language.ErrorParamFieldSelector)
 	}
-	listOptions.FieldSelector = fieldSelector
 
-	limit, ok := params[limIt].(int)
-	if !ok {
-		return listOptions, fmt.Errorf(language.ErrorParamLimit)
+	limit, err := getParamAsInt64(params, limIt)
+	if err != nil {
+		return v1.ListOptions{}, fmt.Errorf(language.ErrorParamLimit)
 	}
-	listOptions.Limit = int64(limit)
+
+	listOptions := v1.ListOptions{
+		LabelSelector: labelSelector,
+		FieldSelector: fieldSelector,
+		Limit:         limit,
+	}
 
 	return listOptions, nil
+}
+
+// getParamAsString retrieves a string value from a map based on a key.
+// It returns an error if the key is not present or the value is not a string.
+//
+// params - a map of parameters where the key is expected to be associated with a string value.
+// key - the key for which to retrieve the string value.
+//
+// Returns the string value and nil on success, or an empty string and an error on failure.
+func getParamAsString(params map[string]interface{}, key string) (string, error) {
+	value, ok := params[key].(string)
+	if !ok {
+		return "", fmt.Errorf(language.ErrorParameterMustBeString, key)
+	}
+	return value, nil
+}
+
+// getParamAsInt64 retrieves an integer value from a map based on a key.
+// It handles both int and float64 data types due to the way JSON and YAML unmarshal numbers.
+// It returns an error if the key is not present or the value is not a number.
+//
+// params - a map of parameters where the key is expected to be associated with an integer value.
+// key - the key for which to retrieve the integer value.
+//
+// Returns the int64 value and nil on success, or 0 and an error on failure.
+func getParamAsInt64(params map[string]interface{}, key string) (int64, error) {
+	if value, ok := params[key].(int); ok {
+		return int64(value), nil
+	}
+	if value, ok := params[key].(float64); ok {
+		return int64(value), nil
+	}
+	return 0, fmt.Errorf(language.ErrorParameterMustBeInteger, key)
 }
